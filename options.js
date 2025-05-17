098a51d7164798e77214f77981585776339a5f03
erolcanulutas <erolcanulutas@gmail.com>
2025-05-18 00:39:04 +0300

    Added imdb and trakt support

--- options.js
+++ options.js
@@ -1,15 +1,35 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const checkbox = document.getElementById("useStremioApp");
-  const saveButton = document.getElementById("save");
+function saveOptions() {
+  const useStremioApp = document.querySelector('#stremioApp').checked;
+  const showOnImdb = document.querySelector('#showOnImdb').checked;
+  const showOnTrakt = document.querySelector('#showOnTrakt').checked;
+  const showOnGoogle = document.querySelector('#showOnGoogle').checked;
 
-  chrome.storage.local.get("useStremioApp", (result) => {
-    checkbox.checked = result.useStremioApp || false;
+  chrome.storage.local.set({
+    useStremioApp: useStremioApp,
+    showOnImdb: showOnImdb,
+    showOnTrakt: showOnTrakt,
+    showOnGoogle: showOnGoogle
   });
 
-  saveButton.addEventListener("click", () => {
-    const useApp = checkbox.checked;
-    chrome.storage.local.set({ useStremioApp: useApp }, () => {
-      alert("Preferences saved!");
-    });
+  const status = document.getElementById('status');
+  status.textContent = 'Options saved.';
+  setTimeout(() => { status.textContent = ''; }, 2000);
+}
+
+function restoreOptions() {
+  chrome.storage.local.get({
+    useStremioApp: null,
+    showOnImdb: true,
+    showOnTrakt: true,
+    showOnGoogle: true
+  }, (result) => {
+    document.querySelector('#stremioApp').checked = result.useStremioApp === true;
+    document.querySelector('#stremioWeb').checked = result.useStremioApp === false || result.useStremioApp === null;
+    document.querySelector('#showOnImdb').checked = result.showOnImdb;
+    document.querySelector('#showOnTrakt').checked = result.showOnTrakt;
+    document.querySelector('#showOnGoogle').checked = result.showOnGoogle;
   });
-});
+}
+
+document.addEventListener('DOMContentLoaded', restoreOptions);
+document.querySelector('#save').addEventListener('click', saveOptions);
